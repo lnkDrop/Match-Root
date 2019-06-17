630e283caf83ca37a8861cbc0820ba6047a1669b
20161152189 <1933341059@qq.com>
2019-06-17 14:57:08 +0800

    update

---
+++ Uploadfiles/可行性研究+面向对象陈红20161152189/单词词频.py
@@ -0,0 +1,21 @@
+﻿import re
+punct = set(u''':!),.:;?]}¢'"、。〉》」』】〕〗〞︰︱︳﹐､﹒
+﹔﹕﹖﹗﹚﹜﹞！），．：；？｜｝︴︶︸︺︼︾﹀﹂﹄﹏､～￠
+々‖•·ˇˉ―--′’”([{£¥'"‵〈《「『【〔〖（［｛￡￥〝︵︷︹︻
+︽︿﹁﹃﹙﹛﹝（｛“‘-—_…''')
+
+word_count_list={}
+with open("in.txt",'r') as f:
+    for line in f:
+        #print('去除标点前：',line)
+        line2 = ''.join(filter(lambda x: x not in punct, line))
+        #print('去除标点后：', line2)
+        wordlist=line2.strip().split()
+        for word in wordlist:
+            #print('({})'.format(word),end='')
+            if word not in word_count_list.keys():
+                word_count_list[word]=1
+            else:
+                word_count_list[word]+=1
+
+print(word_count_list)
